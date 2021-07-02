21505f8206b89518308491521299f89876d449a7
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-07-02 19:29:26 +0300

    feat: install webpack to the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
+import { Article } from './js/Article.js'
+
+const data = [
+  {
+    id: 1,
+    title: 'Increasing Prosperity With Positive Thinking',
+    urlToImage: './src/img/strategies/1.jpg',
+    tags: ['Art', 'Design'],
+    content: 'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
+    date: '01.01.2020'
+  },
+  {
+    id: 2,
+    title: 'Motivation Is The First Step To Success',
+    urlToImage: './src/img/strategies/2.jpg',
+    tags: ['Culture'],
+    content: 'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
+    date: '01.01.2020'
+  },
+  {
+    id: 3,
+    title: 'Success Steps For Your Personal Or Business Life',
+    urlToImage: './src/img/strategies/3.jpg',
+    tags: ['Art', 'Design', 'Culture'],
+    content: 'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
+    date: '01.01.2020'
+  },
+  {
+    id: 4,
+    title: 'Success Steps For Your Personal Or Business Life Plus Funny Image on the Back',
+    urlToImage: './src/img/strategies/4.jpg',
+    tags: ['Art', 'Design', 'Culture'],
+    content: 'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
+    date: '01.01.2020'
+  },
+  {
+    id: 5,
+    title: 'Increasing Prosperity With Positive Thinking',
+    urlToImage: './src/img/strategies/5.jpg',
+    tags: ['Design'],
+    content: 'Your About Us page is vital. It’s often the first stop in any user’s journey through a website or blog. It also shouldn’t be their last, because first impressions count online just as much as they do in the real world. If your visitors aren’t impressed, you can expect them to leave without reading your awesome content or completing a conversion action (e.g., signing up for your newsletter, making a purchase).',
+    date: '01.01.2020'
+  },
+]
+
+
 window.onload = function () {
-  console.log("Hello World!");
+
+  //Render Articles
+  if (data) {
+    renderArticlesToDom()
+  }
 
   //Tags
   addTagsClickHandler();
@@ -50,4 +100,8 @@ const filterStrategiesBySelectedTag = (selectedTag) => {
       }
     })
   })
+}
+
+const renderArticlesToDom = () => {
+
 }
